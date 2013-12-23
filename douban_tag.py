a959876c1530447bb72e74d8ee819698acf356e6
zhulin <zhulin3141@gmail.com>
2013-12-23 23:43:00 +0800

    调整json文件数据生成的算法

--- douban_tag.py
+++ douban_tag.py
@@ -11,21 +11,20 @@ response = urllib.request.urlopen(req)
 soup = BeautifulSoup(response.read().decode('utf8'))
 list_cnt = soup.select('.article div')[1]
 
-data = {}
+data = {"name": "douban","children":[]}
 for li in list_cnt.select('div'):
     group = BeautifulSoup(str(li.select('a[name]'))).a.get('name')
     td = BeautifulSoup(str(li.select('table td')))
     tag = BeautifulSoup(str(td.select('a')))
     num = BeautifulSoup(str(td.select('b')))
 
+    cate = {"name":group,"children":[]}
     item = {}
     for (t,n) in zip(tag,num):
         if t.string.strip() not in ('[',']',','):
-            item[t.string] = n.string.lstrip('(').rstrip(')')
+            item = {"name": t.string, "size": n.string.lstrip('(').rstrip(')')}
+            cate["children"].append(item)
+    data["children"].append(cate)
 
-    data[group] = item
-
-
-print(data)
 open('douban.json','w').write(json.dumps(data))
 
